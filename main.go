4ca95b5c849bcf52e6a2fc797bbdc85640625ea3
我如天翼 <wrtyis@outlook.com>
2022-03-08 16:22:22 +0800

    feat: getClipboardItem

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/glebarez/sqlite"
@@ -34,6 +35,7 @@ func main() {
 		})
 	})
 	api.POST("/ClipboardItem", insertClipboardItem)
+	api.GET("/ClipboardItem", getClipboardItem)
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
 
@@ -46,4 +48,56 @@ func insertClipboardItem(c *gin.Context) {
 	}
 	db.Create(&item)
 	c.JSON(http.StatusOK, item)
+}
+
+func getClipboardItem(c *gin.Context) {
+
+	var startTimestamp int64
+	var endTimestamp int64
+	var limit int
+
+	_start_timestamp := c.Query("startTimestamp")
+	_end_timestamp := c.Query("endTimestamp")
+	_limit := c.Query("limit")
+
+	items := []ClipboardItem{}
+
+	if _limit == "" {
+		limit = 100
+	} else {
+		limit, err = strconv.Atoi(_limit)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	tx := db.Limit(limit)
+
+	if _start_timestamp != "" {
+		t, err := strconv.Atoi(_end_timestamp)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		startTimestamp = int64(t)
+		tx = tx.Where("ClipboardItemTime > ?", startTimestamp)
+	}
+
+	if _end_timestamp != "" {
+		t, err := strconv.Atoi(_end_timestamp)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		endTimestamp = int64(t)
+		tx.Where("ClipboardItemTime <= ?", endTimestamp)
+	}
+
+	tx.Find(&items)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, items)
 }

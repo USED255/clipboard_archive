10915e6c95d9d3205f8813df6d18a425b8c5e3b2
我如天翼 <wrtyis@outlook.com>
2022-03-14 20:46:18 +0800

    更新

--- main.go
+++ main.go
@@ -235,7 +235,7 @@ func webServer(bindFlagPtr *string) {
 	})
 	api.POST("/ClipboardItem", insertClipboardItem)
 	api.GET("/ClipboardItem", getClipboardItem)
-	err = r.Run(*bindFlagPtr) // 监听并在 0.0.0.0:8080 上启动服务
+	err = r.Run(*bindFlagPtr)
 	if err != nil {
 		log.Fatal(err)
 	}

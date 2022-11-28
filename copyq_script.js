3ca6fd0b01830a634e1f95d8804ea00f13b32ac4
我如天翼 <wrtyis@outlook.com>
2022-11-28 17:23:38 +0800

    更新 copyq_script.js

--- copyq_script.js
+++ copyq_script.js
@@ -1,3 +1,4 @@
+// https://copyq.readthedocs.io/en/latest/scripting-api.html
 copyq:
 
 var minBytes = 250 * 1000;
@@ -14,18 +15,26 @@ function hasBigData() {
     return false
 }
 
+function clipboardItem(Item) {
+    ClipboardItemData = toBase64(pack(Item));
+    ClipboardItemHash = sha256sum(ClipboardItemData);
+    ClipboardItemTime = parseInt(str(Item["application/x-copyq-user-copy-time"]));
+    ClipboardItemText = str(Item[mimeText]);
+    ClipboardItemObject = {
+        "ClipboardItemTime": ClipboardItemTime,
+        "ClipboardItemText": ClipboardItemText,
+        "ClipboardItemHash": ClipboardItemHash,
+        "ClipboardItemData": ClipboardItemData
+    };
+    return JSON.stringify(ClipboardItemObject);
+}
+
 function main() {
     if (hasBigData()) {
         return;
     }
-    ClipboardItem = getItem(0);
-    ClipboardItemData = toBase64(pack(ClipboardItem));
-    ClipboardItemHash = sha256sum(ClipboardItemData);
-    ClipboardItemTime = parseInt(str(ClipboardItem["application/x-copyq-user-copy-time"]));
-    ClipboardItemText = str(ClipboardItem[mimeText]);
-    ClipboardItemObject = { "ClipboardItemTime": ClipboardItemTime, "ClipboardItemText": ClipboardItemText, "ClipboardItemHash": ClipboardItemHash, "ClipboardItemData": ClipboardItemData };
-    ClipboardItemJson = JSON.stringify(ClipboardItemObject);
-    networkPost(url, ClipboardItemJson).data;
+    ClipboardItem = clipboardItem(getItem(0));
+    networkPost(url, ClipboardItem).data;
 }
 
 main()

b8057801eb525223f78c843eeeea0d9ad8d2956c
我如天翼 <wrtyis@outlook.com>
2022-11-28 17:31:19 +0800

    调整排版

--- database/database_test.go
+++ database/database_test.go
@@ -6,15 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCloseDatabase(t *testing.T) {
-	ConnectDatabase("file::memory:?cache=shared")
-	CloseDatabase()
-}
-
 func TestConnectDatabase(t *testing.T) {
 	ConnectDatabase("file::memory:?cache=shared")
 
 	assert.NotNil(t, Orm)
 
+	CloseDatabase()
+}
+
+func TestCloseDatabase(t *testing.T) {
+	ConnectDatabase("file::memory:?cache=shared")
 	CloseDatabase()
 }
